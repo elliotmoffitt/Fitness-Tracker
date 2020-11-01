8c4ccb04637dad4eb59f326b0614edbda3707c23
Elliot Moffitt <elliot.winton@gmail.com>
2020-11-01 18:48:57 -0500

    server.js port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost/workout", {
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
